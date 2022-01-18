0aafd3f0af88d8896032d5517be0e6e04ae023f0
Charlyboy1993 <cjaramilloportilla@gmail.com>
2022-01-18 14:01:19 -0500

    Se integra los estilos css

--- app.js
+++ app.js
@@ -4,7 +4,9 @@ const app = express();
 
 //const publicPath = path.resolve(__dirname, './public');
 // app.use( express.static(publicPath));
-app.use('/static', express.static(__dirname + '/public'));
+//app.use('/static', express.static(__dirname + '/public'));
+app.use(express.static('public'));
+
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, './views/home.html'))
