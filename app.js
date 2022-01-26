d83c1a1f660d9aa0aa8137e83da2130503dd5df4
Charlyboy1993 <cjaramilloportilla@gmail.com>
2022-01-25 19:00:13 -0500

    Se crea las rutas de accceso de login y signup

--- app.js
+++ app.js
@@ -12,6 +12,14 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, './views/home.html'))
 });
 
+app.get('/sign-up', (req, res) => {
+    res.sendFile(path.join(__dirname, './views/register.html'));
+});
+
+app.get('/login', (req, res) => {
+    res.sendFile(path.join(__dirname, './views/login.html'));
+});
+
 app.listen(3000, () => {
     console.log('Servidor corriendo en el puerto 3000')
 });
