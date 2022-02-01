8841b7fd424e07599c0b8a7c3381e2386ce0f77c
Charlyboy1993 <cjaramilloportilla@gmail.com>
2022-02-01 13:15:20 -0500

    Ajustes para deploy en heroku

--- app.js
+++ app.js
@@ -20,10 +20,6 @@ app.get('/login', (req, res) => {
     res.sendFile(path.join(__dirname, './views/login.html'));
 });
 
-app.get('/banner', (req, res) => {
-    res.sendFile(path.join(__dirname, './views/banner.html'));
-});
-
-app.listen(3000, () => {
+app.listen(process.env.PORT || 3000, () => {
     console.log('Servidor corriendo en el puerto 3000')
 });
